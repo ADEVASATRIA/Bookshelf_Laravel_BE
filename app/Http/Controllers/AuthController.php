20d34f8dd828ab850573ebb7b6032deca4490d91
adeva satria arif wibawa <adevawibawa@gmail.com>
2024-02-29 12:44:33 +0700

    [Add] Success Configure Register, Login, & Get All Data Users

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            'message' => 'User successfully registered',
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('AuthToken')->plainTextToken;
+
+            return response()->json([
+                'User' => $user,
+                'Noted' => "Successfully logged in",
+                'Berikut Token yang bisa dipakai' => $token,
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
+    }
+
+    public function getAllUsers()
+    {
+        $users = User::all();
+
+        return response()->json([
+            'users' => $users
+        ], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+
+// Route Auth Controller (Login, Register)
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/users', [AuthController::class, 'getAllUsers']);
+
+
+// Route Get All Users By Authenticated User
+// Route::middleware('auth:api')->get('/users', [AuthController::class, 'getAllUsers']);
